0876bbeda831e6fb801813f4cd33d14ab92dd52c
杨仕明 <shiming.y@qq.com>
2026-08-13 13:18:12 +0800

    fix(channel): remove manual code input (#80)

--- src/features/channels/types.ts
+++ src/features/channels/types.ts
@@ -24,7 +24,6 @@ export type ChannelListData = TableQueryResult<Channel> & { totalPages?: number 
 
 export interface CreateChannel {
   name: string;
-  code: string;
   description?: string | null;
   isActive?: boolean;
 }
